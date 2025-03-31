827775ffdaffa8696661037d06e8a3733bec1a2d
PhuongHo105 <22521160@gm.uit.edu.vn>
2025-03-31 21:54:13 +0700

    Submit Register page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Login() {
     return (
-        <div className="flex items-center mt-10 m-auto h-auto w-3/5 bg-[#FFFFF6] rounded-xl">
+        <div className="flex items-center mt-10 m-auto h-auto w-3/5 bg-[#FFFFF6] rounded-xl text-montserrat">
             <Image src="/images/loginimage.jpg" alt="image" width={350} height={400} className="rounded-xl" />
-            <div className="flex justify-center ml-10 mr-10 w-fit gap-5 ">
+            <div className="ml-10 mr-10 w-fit gap-5 ">
                 <form id="login-form" className="w-fit">
                     <h1 className="font-montserrat font-bold text-3xl w-fit">Log in to BadmintonGear</h1>
                     <p className="font-montserrat text-xs mt-3 w-fit">Welcome back! Please enter your details.</p>
@@ -15,6 +15,10 @@ export default function Login() {
                         <a className="absolute right-0 text-[#ff8200] text-xs">Forget Password?</a>
                     </div>
                 </form>
+                <div className="text-center mt-10 w-3/4">Don't you have an account?  
+                    <a href="/register" className="text-[#ff8200] font-bold ml-2">Register</a>
+                </div>
+                
             </div>
         </div>
     )

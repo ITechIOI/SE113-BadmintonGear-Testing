2658128e62c0179b1fa02372322cb0af16f3b497
PhuongHo105 <22521160@gm.uit.edu.vn>
2025-04-22 13:54:49 +0700

    Update home page

--- src/app/page.js
+++ src/app/page.js
@@ -102,11 +102,14 @@ export default function Page() {
 
           </div>
         </div>
+
         {/* --- Discount Banner --- */}
         <div>
           <Image src={"/images/banner.png"} alt={"Discount Banner"} width={1000} height={200} className="w-auto h-auto" />
         </div>
       </div>
+
+      {/* --- Flash Sale --- */}
       <div className="mx-20 border-b border-gray-300">
         <div className="flex items-center py-5">
           <div className="bg-[#FF8200] w-5 h-10 rounded-sm"></div>
@@ -131,16 +134,43 @@ export default function Page() {
         </div>
         <button className="px-10 py-3 mx-auto flex mb-5 bg-[#ff8200] rounded-md text-white">View All Products</button>
       </div>
+
+      {/* --- Category --- */}
       <div className="mx-20 mb-5 border-b border-gray-300">
         <div className="flex items-center py-5">
           <div className="bg-[#FF8200] w-5 h-10 rounded-sm"></div>
           <div className="text-[#ff8200] ml-5 font-[600] text-xl">Categories</div>
         </div>
         <p className="text-3xl font-bold mr-20">Browse By Category</p>
-        <div className="mb-5">
-
+        <div className=" flex flex-wrap h-auto mx-auto justify-between my-10 " >
+          <div className="border flex flex-col items-center justify-center px-14 mr-5 py-10 h-auto rounded-md hover:shadow-lg cursor-pointer">
+            <Image src={"/icons/racketic.png"} alt="racket" width={100} height={100} className="mb-5" />
+            <p>Rackets</p>
+          </div>
+          <div className="border flex flex-col items-center justify-center mr-5 px-15 py-10 h-auto rounded-md hover:shadow-lg cursor-pointer">
+            <Image src={"/icons/shuttlecockic.png"} alt="shuttlecock" width={80} height={80} className="mb-5" />
+            <p>Shuttlecock</p>
+          </div>
+          <div className="border flex flex-col items-center justify-center mr-5 px-15 py-10 h-auto rounded-md hover:shadow-lg cursor-pointer">
+            <Image src={"/icons/shoesic.png"} alt="shoes" width={80} height={80} className="mb-5" />
+            <p>Shoes</p>
+          </div>
+          <div className="border flex flex-col items-center justify-center mr-5 px-15 py-10 h-auto rounded-md hover:shadow-lg cursor-pointer">
+            <Image src={"/icons/clotheic.png"} alt="clothes" width={80} height={80} className="mb-5" />
+            <p>Clothes</p>
+          </div>
+          <div className="border flex flex-col items-center justify-center mr-5 px-15 py-10 h-auto rounded-md hover:shadow-lg cursor-pointer">
+            <Image src={"/icons/bagic.png"} alt="bags" width={80} height={80} className="mb-5" />
+            <p>Bags</p>
+          </div>
+          <div className="border flex flex-col items-center justify-center px-15 py-10 h-auto rounded-md hover:shadow-lg cursor-pointer">
+            <Image src={"/icons/otheric.png"} alt="others" width={80} height={80} className="mb-5" />
+            <p>Others</p>
+          </div>
         </div>
       </div>
+
+      {/* --- Best Selling Product --- */}
       <div className="mx-20 mb-5 border-b border-gray-300">
         <div className="flex items-center py-5">
           <div className="bg-[#FF8200] w-5 h-10 rounded-sm"></div>
@@ -157,9 +187,13 @@ export default function Page() {
           <ProductCard product={{ id: "pro004", name: "Product 4", image: "/images/product1.png", price: 180, rating: 4, discount: 50 }} className="min-w-[250px] flex-shrink-0" />
         </div>
       </div>
+
+      {/* --- Banner2 --- */}
       <div className="mx-15">
         <Image src={"/images/banner2.png"} alt={"Discount Banner"} width={1000} height={200} className="w-full h-auto" />
       </div>
+
+      {/* --- Our Products --- */}
       <div className="mx-20 mb-5 border-b border-gray-300">
         <div className="flex items-center py-5">
           <div className="bg-[#FF8200] w-5 h-10 rounded-sm"></div>
@@ -176,6 +210,8 @@ export default function Page() {
           <ProductCard product={{ id: "pro005", name: "Product 5", image: "/images/product1.png", price: 150, rating: 4, discount: 10 }} />
         </div>
       </div>
+
+      {/* --- Service --- */}
       <div className="mt-10 ml-30 mr-30 gap-auto flex items-center justify-center">
         <div id="item" className="w-1/3 p-5 flex flex-col items-center justify-center rounded-sm gap-4 ">
           <Image src="/icons/deliveryic.png" alt="image" width={70} height={70} className="mt-5" />
@@ -188,7 +224,6 @@ export default function Page() {
           <p className="text-center">Friendly 24/7 customer support</p>
         </div>
       </div>
-      {/* <ProductCard product={{id:"pro001", name: "Product 1", image: "/images/product1.png", price: 166, rating: 4 , discount: 40}} /> */}
     </div>
   )
 }

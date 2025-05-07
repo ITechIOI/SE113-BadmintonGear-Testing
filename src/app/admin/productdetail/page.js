6820f698bb53f4c125f925ac979e09e9f40235ab
PhuongHo105 <22521160@gm.uit.edu.vn>
2025-05-08 00:06:44 +0700

    Submit customer detail page

--- src/app/admin/productdetail/page.js
+++ src/app/admin/productdetail/page.js
@@ -84,7 +84,7 @@ export default function ProductDetail(id) {
             {/* {setImagePreviews(product.image)} */}
             <div className='flex justify-between items-end'>
                 <div>
-                    <h1 className='text-3xl font-bold'>Add Product</h1>
+                    <h1 className='text-3xl font-bold'>Product Details</h1>
                     <div id="roadmap" className="flex items-center mt-2">
                         <a className="text-[#ff8200]" href="/admin/dashboard">Dashboard</a>
                         <label className="ml-3 mr-3">
@@ -98,7 +98,7 @@ export default function ProductDetail(id) {
                                 <path fillRule="evenodd" clipRule="evenodd" d="M6.59467 3.96967C6.30178 4.26256 6.30178 4.73744 6.59467 5.03033L10.5643 9L6.59467 12.9697C6.30178 13.2626 6.30178 13.7374 6.59467 14.0303C6.88756 14.3232 7.36244 14.3232 7.65533 14.0303L12.4205 9.26516C12.5669 9.11872 12.5669 8.88128 12.4205 8.73484L7.65533 3.96967C7.36244 3.67678 6.88756 3.67678 6.59467 3.96967Z" fill="#A3A9B6" />
                             </svg>
                         </label>
-                        <a className="text-[#667085]" href="/admin/addproduct">Add Product</a>
+                        <a className="text-[#667085]" href="/admin/addproduct">Product Details</a>
                     </div>
 
                 </div>

8dcc930ac47c684ceb268138afcda759b9ad96d7
PhuongHo105 <22521160@gm.uit.edu.vn>
2025-06-01 00:29:18 +0700

    promotion and promotion detail

---
+++ src/api/promotionApi.js
@@ -0,0 +1,136 @@
+const getAllPromotions = async () => {
+    try {
+        const url = `${process.env.NEXT_PUBLIC_API_URL}/orders/discounts/all`;
+        const token = localStorage.getItem('access_token');
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            },
+        });
+        if (!response.ok) {
+            console.log('Failed to fetch promotions');
+            return null;
+        }
+        const data = await response.json();
+        return data.data.content;
+    } catch (error) {
+        console.error('Error fetching promotions:', error);
+        return null;
+    }
+}
+
+const getPromotionById = async (promotionId) => {
+    try {
+        const url = `${process.env.NEXT_PUBLIC_API_URL}/orders/discounts/id/${promotionId}`;
+        const token = localStorage.getItem('access_token');
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            },
+        });
+        if (!response.ok) {
+            console.log('Failed to fetch promotion by ID');
+            return null;
+        }
+        const data = await response.json();
+        return data.data;
+    } catch (error) {
+        console.error('Error fetching promotion by ID:', error);
+        return null;
+    }
+}
+
+const getPromotionByCode = async (code) => {
+    try {
+        const url = `${process.env.NEXT_PUBLIC_API_URL}/orders/discounts/code/${code}`;
+        const token = localStorage.getItem('access_token');
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            },
+        });
+        if (!response.ok) {
+            console.log('Failed to fetch promotion by code');
+            return null;
+        }
+        const data = await response.json();
+        return data.data;
+    } catch (error) {
+        console.error('Error fetching promotion by code:', error);
+        return null;
+    }
+}
+
+const deletePromotionById = async (promotionId) => {
+    try {
+        const url = `${process.env.NEXT_PUBLIC_API_URL}/orders/discounts/${promotionId}`;
+        const token = localStorage.getItem('access_token');
+        const response = await fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            },
+        });
+        if (!response.ok) {
+            console.log('Failed to delete promotion');
+            return false;
+        }
+        return true;
+    } catch (error) {
+        console.error('Error deleting promotion:', error);
+        return false;
+    }
+}
+
+const addPromotion = async (promotionData) => {
+    try {
+        const url = `${process.env.NEXT_PUBLIC_API_URL}/orders/discounts/new`;
+        const token = localStorage.getItem('access_token');
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(promotionData),
+        });
+        if (!response.ok) {
+            console.log('Failed to add promotion');
+            return null;
+        }
+        const data = await response.json();
+        return data.data;
+    } catch (error) {
+        console.error('Error adding promotion:', error);
+        return null;
+    }
+}
+
+const updatePromotion = async (promotionId, promotionData) => {
+    try {
+        const url = `${process.env.NEXT_PUBLIC_API_URL}/orders/discounts/${promotionId}`;
+        const token = localStorage.getItem('access_token');
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(promotionData),
+        });
+        if (!response.ok) {
+            console.log('Failed to update promotion');
+            return null;
+        }
+        const data = await response.json();
+        return data.data;
+    } catch (error) {
+        console.error('Error updating promotion:', error);
+        return null;
+    }
+}
+
+export { getAllPromotions, getPromotionById, getPromotionByCode, deletePromotionById, addPromotion, updatePromotion };
--- src/app/admin/promotion/page.js
+++ src/app/admin/promotion/page.js
@@ -1,32 +1,126 @@
 "use client"
-import React, {useState} from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import AdminPromotionItem from '@/components/AdminPromotionItem';
+import { getAllPromotions, deletePromotionById } from '@/api/promotionApi';
+import { useRouter } from 'next/navigation';
 
 export default function PromotionPage() {
     const [isAllChecked, setIsAllChecked] = useState(false); // Trạng thái checkbox của thead
-    const [promotions, setPromotions] = useState([
-        {id: 1, code: 'CODE1', discount: 10, startDate: '2023-01-01', endDate: '2023-12-31', status: 'Active', isChecked: false },
-        {id: 2, code: 'CODE2', discount: 20, startDate: '2023-02-01', endDate: '2023-11-30', status: 'Out of turn', isChecked: false },
-        {id: 3, code: 'CODE3', discount: 30, startDate: '2023-03-01', endDate: '2023-10-31', status: 'Inactive', isChecked: false },
-    ]);
+    const [promotions, setPromotions] = useState([]);
+    const [displayedPromotions, setDisplayedPromotions] = useState([]);
+    const [isCheck, setIsCheck] = useState(false);
+    const [showFilter, setShowFilter] = useState(false);
+    const router = useRouter();
+
+    const filterRef = useRef(null);
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (filterRef.current && !filterRef.current.contains(event.target)) {
+                setShowFilter(false);
+            }
+        }
+        if (showFilter) {
+            document.addEventListener("mousedown", handleClickOutside);
+        } else {
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [showFilter]);
 
     const handleSelectAll = () => {
         const newCheckedState = !isAllChecked;
         setIsAllChecked(newCheckedState);
         setPromotions(promotions.map(promotion => ({ ...promotion, isChecked: newCheckedState })));
+        setDisplayedPromotions(promotions.map(promotion => ({ ...promotion, isChecked: newCheckedState })));
     };
 
-    // Hàm xử lý khi checkbox trong tbody được chọn
     const handlePromotionCheck = (id) => {
         const updatedPromotions = promotions.map(promotion =>
             promotion.id === id ? { ...promotion, isChecked: !promotion.isChecked } : promotion
         );
         setPromotions(updatedPromotions);
-
-        // Kiểm tra nếu tất cả các checkbox con đều được chọn
+        setDisplayedPromotions(updatedPromotions);
         const allChecked = updatedPromotions.every(promotion => promotion.isChecked);
         setIsAllChecked(allChecked);
     };
+
+    const fetchPromotions = async () => {
+        const data = await getAllPromotions();
+        if (data) {
+            console.log('Fetched promotions:', data);
+            setPromotions(data.map(promotion => ({ ...promotion, isChecked: false })));
+            setDisplayedPromotions(data.map(promotion => ({ ...promotion, isChecked: false })));
+        } else {
+            console.error('Failed to fetch promotions');
+        }
+    }
+
+    const fetchDeletePromotion = async (id) => {
+        const response = await deletePromotionById(id);
+        if (response) {
+            fetchPromotions(); // Refresh the list after deletion
+            return true;
+        } else {
+            console.error('Failed to delete promotion');
+            return false;
+        }
+    }
+
+    const handleDeletePromotion = (id) => {
+        if (window.confirm("Are you sure you want to delete this promotion?")) {
+            fetchDeletePromotion(id);
+        }
+    }
+
+    const handleFilterChange = (e) => {
+        const value = e.target.value;
+        if (value === "") {
+            setDisplayedPromotions(promotions);
+        } else {
+            const filteredPromotions = promotions.filter(promotion => promotion.status === value);
+            setDisplayedPromotions(filteredPromotions);
+        }
+    };
+
+    const handleDeleteCheckedPromotions = () => {
+        const checkedPromotions = promotions.filter(promotion => promotion.isChecked);
+        if (checkedPromotions.length === 0) {
+            alert("No promotions selected for deletion.");
+            return;
+        }
+        if (window.confirm(`Are you sure you want to delete ${checkedPromotions.length} promotions?`)) {
+            checkedPromotions.forEach(promotion => {
+                if (fetchDeletePromotion(promotion.id)) {
+                    console.log(`Deleted promotion with ID: ${promotion.id}`);
+                }
+                else {
+                    console.log(`Failed to delete promotion with ID: ${promotion.id}`);
+                }
+            });
+        }
+    }
+
+    const handleSearchChange = (e) => {
+        const searchTerm = e.target.value.toLowerCase();
+        const filteredPromotions = promotions.filter(promotion =>
+            promotion.code.toLowerCase().includes(searchTerm) ||
+            promotion.id.toString().includes(searchTerm)
+        );
+        setDisplayedPromotions(filteredPromotions);
+    }
+
+    useEffect(() => {
+        fetchPromotions();
+    }, []);
+
+    useEffect(() => {
+        const anyChecked = promotions.some(promotion => promotion.isChecked);
+        setIsCheck(anyChecked);
+        setDisplayedPromotions(promotions);
+    }, [promotions]);
 
     return (
         <div className='font-inter'>
@@ -51,12 +145,22 @@ export default function PromotionPage() {
                         </svg>
                         Export</button>
                     <button className='bg-[#ff8200] text-white px-4 py-2 rounded-md flex gap-2 items-center'
-                        >
+                        onClick={() => { router.push('/admin/promotiondetail?mode=add') }}>
                         <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M9.16667 15.4167C9.16667 15.8769 9.53976 16.25 10 16.25C10.4602 16.25 10.8333 15.8769 10.8333 15.4167V10.8333H15.4167C15.8769 10.8333 16.25 10.4602 16.25 10C16.25 9.53976 15.8769 9.16667 15.4167 9.16667H10.8333V4.58333C10.8333 4.1231 10.4602 3.75 10 3.75C9.53976 3.75 9.16667 4.1231 9.16667 4.58333V9.16667H4.58333C4.1231 9.16667 3.75 9.53976 3.75 10C3.75 10.4602 4.1231 10.8333 4.58333 10.8333H9.16667V15.4167Z" fill="white" />
                         </svg>
                         Add Promotion
                     </button>
+                    {isCheck && (
+                        <button className='bg-[#ff8200] text-white px-4 py-2 rounded-md flex gap-2 items-center' onClick={handleDeleteCheckedPromotions}>
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M8.33317 8.12484C8.79341 8.12484 9.1665 8.49793 9.1665 8.95817V13.9582C9.1665 14.4184 8.79341 14.7915 8.33317 14.7915C7.87293 14.7915 7.49984 14.4184 7.49984 13.9582V8.95817C7.49984 8.49793 7.87293 8.12484 8.33317 8.12484Z" fill="#fff" />
+                                <path d="M12.4998 8.95817C12.4998 8.49793 12.1267 8.12484 11.6665 8.12484C11.2063 8.12484 10.8332 8.49793 10.8332 8.95817V13.9582C10.8332 14.4184 11.2063 14.7915 11.6665 14.7915C12.1267 14.7915 12.4998 14.4184 12.4998 13.9582V8.95817Z" fill="#fff" />
+                                <path fillRule="evenodd" clipRule="evenodd" d="M14.9998 4.99984V4.1665C14.9998 2.78579 13.8806 1.6665 12.4998 1.6665H7.49984C6.11913 1.6665 4.99984 2.78579 4.99984 4.1665V4.99984H3.74984C3.2896 4.99984 2.9165 5.37293 2.9165 5.83317C2.9165 6.29341 3.2896 6.6665 3.74984 6.6665H4.1665V15.8332C4.1665 17.2139 5.28579 18.3332 6.6665 18.3332H13.3332C14.7139 18.3332 15.8332 17.2139 15.8332 15.8332V6.6665H16.2498C16.7101 6.6665 17.0832 6.29341 17.0832 5.83317C17.0832 5.37293 16.7101 4.99984 16.2498 4.99984H14.9998ZM12.4998 3.33317H7.49984C7.0396 3.33317 6.6665 3.70627 6.6665 4.1665V4.99984H13.3332V4.1665C13.3332 3.70627 12.9601 3.33317 12.4998 3.33317ZM14.1665 6.6665H5.83317V15.8332C5.83317 16.2934 6.20627 16.6665 6.6665 16.6665H13.3332C13.7934 16.6665 14.1665 16.2934 14.1665 15.8332V6.6665Z" fill="#fff" />
+                            </svg>
+                            Delete
+                        </button>
+                    )}
                 </div>
             </div>
             <div className='mt-5 flex justify-between items-center'>
@@ -64,9 +168,9 @@ export default function PromotionPage() {
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd" clipRule="evenodd" d="M14.7844 16.1991C11.646 18.6416 7.10629 18.4205 4.22156 15.5358C1.09737 12.4116 1.09737 7.34625 4.22156 4.22205C7.34576 1.09786 12.4111 1.09786 15.5353 4.22205C18.42 7.10677 18.6411 11.6464 16.1986 14.7849L20.4851 19.0713C20.8756 19.4618 20.8756 20.095 20.4851 20.4855C20.0945 20.876 19.4614 20.876 19.0708 20.4855L14.7844 16.1991ZM5.63578 14.1215C7.97892 16.4647 11.7779 16.4647 14.1211 14.1215C16.4642 11.7784 16.4642 7.97941 14.1211 5.63627C11.7779 3.29312 7.97892 3.29312 5.63578 5.63627C3.29263 7.97941 3.29263 11.7784 5.63578 14.1215Z" fill="#667085" />
                     </svg>
-                    <input type='text' placeholder='Search customer...' className='px-2 py-2 outline-none' />
+                    <input type='text' placeholder='Search promotion...' className='px-2 py-2 outline-none' onChange={handleSearchChange} />
                 </div>
-                <button className='text-[#667085] border border-[#E0E2E7] bg-white rounded-md px-4 py-2 flex items-center gap-2'>
+                <button className='text-[#667085] border border-[#E0E2E7] bg-white rounded-md px-4 py-2 flex items-center gap-2' onClick={() => setShowFilter(!showFilter)}>
                     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.8333 6.66667C10.8333 7.1269 11.2064 7.5 11.6667 7.5C12.1269 7.5 12.5 7.1269 12.5 6.66667V5.83333H16.6667C17.1269 5.83333 17.5 5.46024 17.5 5C17.5 4.53976 17.1269 4.16667 16.6667 4.16667H12.5V3.33333C12.5 2.8731 12.1269 2.5 11.6667 2.5C11.2064 2.5 10.8333 2.8731 10.8333 3.33333V6.66667Z" fill="#667085" />
                         <path d="M2.5 10C2.5 9.53976 2.8731 9.16667 3.33333 9.16667H4.58333C4.81345 9.16667 5 9.35321 5 9.58333V10.4167C5 10.6468 4.81345 10.8333 4.58333 10.8333H3.33333C2.8731 10.8333 2.5 10.4602 2.5 10Z" fill="#667085" />
@@ -77,11 +181,23 @@ export default function PromotionPage() {
                     </svg>
                     Filters
                 </button>
+                {showFilter && (
+                    <div ref={filterRef} className='absolute top-16 right-5 bg-white shadow-lg rounded-md p-4 border border-[#E0E2E7]'>
+                        <h3 className='font-semibold mb-2'>Filter Promotions</h3>
+                        <select className='w-full p-2 border border-[#E0E2E7] rounded-md' onChange={handleFilterChange} defaultValue="">
+                            <option value="">All Promotions</option>
+                            <option value="available">Available Promotions</option>
+                            <option value="disable">Disable Promotions</option>
+                            <option value="expired">Expired Promotions</option>
+                            <option value="out of turn">Out of Turn Promotions</option>
+                        </select>
+                    </div>
+                )}
             </div>
             <div className="shadow-md rounded-md border border-[#E0E2E7] mt-5">
                 <table className='w-full py-2 rounded-md overflow-hidden '>
                     <thead className='bg-[#F9F9FC] font-medium border-b border-[#F0F1F3]'>
-                        <tr className='text-left text-[#344054] font-semibold rounded-md'>
+                        <tr className='text-center text-[#344054] font-semibold rounded-md'>
                             <th>
                                 <input
                                     type='checkbox'
@@ -93,18 +209,21 @@ export default function PromotionPage() {
                             <th className='py-2 px-4'>Promotion ID</th>
                             <th className='py-2 px-4'>Code</th>
                             <th className='py-2 px-4'>Discount</th>
+                            <th className='py-2 px-4'>Min Order Value</th>
+                            <th className='py-2 px-4'>Quantity</th>
                             <th className='py-2 px-4'>Start Date</th>
                             <th className='py-2 px-4'>End Date</th>
                             <th className='py-2 px-4'>Status</th>
                             <th className='py-2 px-4'>Action</th>
                         </tr>
                     </thead>
-                    <tbody className='text-[#344054] font-normal'>
-                        {promotions.map((promotion) => (
+                    <tbody className='text-[#344054] font-normal text-center'>
+                        {displayedPromotions.map((promotion) => (
                             <AdminPromotionItem
                                 key={promotion.id}
                                 promotion={promotion}
-                                onCheck={() => handlePromotionCheck(promotion.id)} />
+                                onCheck={() => handlePromotionCheck(promotion.id)}
+                                onDelete={() => handleDeletePromotion(promotion.id)} />
                         ))}
                     </tbody>
                 </table>
